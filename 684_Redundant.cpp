2406dafc7ba63da7dca88dd506805d798fae422e
John Lee <tmdgjs2592@gmail.com>
2023-10-09 23:22:30 -0700

    #684. Redundant Connection

--- 684_Redundant.cpp
+++ 684_Redundant.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
 using namespace std;
+
+
+
+
+
+
 
 int main()
 {
+    int N;
+    cin >> N;
+    vector<int> m;
+    for(int i=0; i<N; i++){
+        int l;
+        cin >> l;
+        m.push_back(l);
+    }
+    int res = 0;
+    for(int i=0; i<N; i++){
+        res += m[i];
+    }
+    res -= N;
+    cout << res;
+
+
 
     return 0;
 }
